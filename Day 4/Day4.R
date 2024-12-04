62278aa29d8df38a2d6a2a7cbe4eb5eb9503cf75
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-04 13:58:41 -0500

    simplified horizontal counting

--- Day 4/Day4.R
+++ Day 4/Day4.R
@@ -1,6 +1,6 @@
-input_filename <- "input.txt"
+input_filename <- "sample.txt"
 input <- read.table(input_filename)[[1]]
 gridsize <- nchar(input[1])
 
-num_xmas <- sum(unlist(lapply(gregexpr("XMAS", input), length)))
-num_samx <- sum(unlist(lapply(gregexpr("SAMX", input), length)))
+num_xmas <- sum(gregexpr("XMAS", input) != -1)
+num_samx <- sum(gregexpr("SAMX", input) != -1)
