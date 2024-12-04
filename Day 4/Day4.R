79fcfccbe2ad59f365e7691af7e75640d53231c0
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-04 13:52:09 -0500

    Counting xmas forward and backward horizontally

--- Day 4/Day4.R
+++ Day 4/Day4.R
@@ -2,8 +2,5 @@ input_filename <- "input.txt"
 input <- read.table(input_filename)[[1]]
 gridsize <- nchar(input[1])
 
-x <- gregexpr("X", input)
-
-for (i in seq_len(gridsize)) {
-  
-}
+num_xmas <- sum(unlist(lapply(gregexpr("XMAS", input), length)))
+num_samx <- sum(unlist(lapply(gregexpr("SAMX", input), length)))
