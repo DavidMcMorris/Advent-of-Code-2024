53376302075fb49a15c1f4380035348bfd5b3b27
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-03 19:45:11 -0500

    Finished part 2

--- Day 3/Day3.R
+++ Day 3/Day3.R
@@ -3,13 +3,32 @@ library(dplyr)
 input_filename <- "input.txt"
 input <- readLines(input_filename)
 
-mulls <- regmatches(input,gregexpr("mul\\(\\K\\d+,\\d+(?=\\))", input, perl=TRUE))[[1]]
+mul_inds <- gregexpr("mul\\(\\K\\d+,\\d+(?=\\))", input, perl = TRUE)
+muls <- regmatches(input, mul_inds)[[1]]
+mul_inds <- as.vector(mul_inds[[1]])
+dos <- as.vector(gregexpr("do\\(\\)", input, perl = TRUE)[[1]])
+donts <- as.vector(gregexpr("don't\\(\\)", input, perl = TRUE)[[1]])
 
-mulls <- mulls %>%
+muls <- muls %>%
   strsplit(., ",") %>%
   unlist() %>%
   as.numeric() %>%
   matrix(., ncol = 2, byrow = TRUE)
 
-mul_sum <- sum(mulls[, 1] * mulls[, 2])
-print(mul_sum)
+pt1_mul_sum <- sum(muls[, 1] * muls[, 2])
+print(pt1_mul_sum)
+
+current_instruction <- 1
+pt2_mul_sum <- 0
+for (i in sort(c(mul_inds, dos, donts))) {
+  if (i %in% dos) {
+    current_instruction <- 1
+  } else if (i %in% donts) {
+    current_instruction <- 0
+  } else {
+    j <- which(mul_inds == i)
+    pt2_mul_sum <- pt2_mul_sum + current_instruction * (muls[j, 1] * muls[j, 2])
+  }
+}
+
+print(pt2_mul_sum)
