d0d29215219771e0f45f76fa59a1f71613d9ad5d
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-05 13:33:01 -0500

    Finished part 1

--- Day 4/Day4.R
+++ Day 4/Day4.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-input_filename <- "sample.txt"
+input_filename <- "input.txt"
 input <- read.table(input_filename)[[1]]
 gridsize <- nchar(input[1])
 
@@ -18,16 +18,48 @@ input_v <- input %>%
 num_xmas_v <- sum(sapply(gregexpr("XMAS", input_v), function(x) sum(x != -1)))
 num_samx_v <- sum(sapply(gregexpr("SAMX", input_v), function(x) sum(x != -1)))
 
-print(num_xmas_h + num_samx_h + num_xmas_v + num_samx_v)
-
 # Split strings for diagonal search
-# input <- input %>%
-#   strsplit(., "") %>%
-#   unlist() %>%
-#   matrix(., nrow = gridsize, byrow = TRUE)
+input <- input %>%
+  strsplit(., "") %>%
+  unlist() %>%
+  matrix(., nrow = gridsize, byrow = TRUE)
 
-# x_inds <- which(input == "X")
+x_inds <- arrayInd(which(input == "X"), rep(gridsize, 2))
+se <- matrix(rep(0:3, 2), ncol = 2)
+sw <- se %*% rbind(c(1, 0), c(0, -1))
+ne <- sw[,c(2,1)]
+nw <- -se
 
-# diag_search <- function(ind) {
+coord_tester <- function(mat) {
+  sum(mat <= 0) == 0 && sum(mat > gridsize) == 0
+}
 
-# }
+word_tester <- function(word) {
+  word == "XMAS" | word == "SAMX"
+}
+
+diag_search <- function(ind) {
+  count <- 0
+  coord_mat <- matrix(rep(ind, 4), ncol = 2, byrow = TRUE)
+  se_coords <- coord_mat + se
+  sw_coords <- coord_mat + sw
+  ne_coords <- coord_mat + ne
+  nw_coords <- coord_mat + nw
+  coord_list <- list(se_coords, sw_coords, ne_coords, nw_coords)
+  for (i in 1:4) {
+    if (coord_tester(coord_list[[i]])) {
+      word <- paste0(input[coord_list[[i]]], collapse = "")
+      if (word_tester(word)) {
+        count <- count + 1
+      }
+    }
+  }
+  return(count)
+}
+
+num_diags <- 0
+for (i in seq_len(nrow(x_inds))) {
+  num_diags <- num_diags + diag_search(x_inds[i, ])
+}
+
+print(num_diags + num_xmas_h + num_samx_h + num_xmas_v + num_samx_v)
