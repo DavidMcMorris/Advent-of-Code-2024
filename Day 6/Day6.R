21101d1382247da107ccdeb6aeadea4a8b67d888
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-06 13:19:24 -0500

    added walk function for part 1

--- Day 6/Day6.R
+++ Day 6/Day6.R
@@ -8,3 +8,17 @@ input <- input %>%
   strsplit(., "") %>%
   unlist() %>%
   matrix(., nrow = gridsize, byrow = TRUE)
+
+obstructions <- arrayInd(which(input == "O"), rep(gridsize, 2))
+direction <- c(-1, 0)
+guard <- arrayInd(which(input == "^"), rep(gridsize, 2))
+turn_mat <- matrix(c(0, -1, 1, 0), nrow = 2)
+
+walk <- function(guard, direction) {
+  if (guard + direction %in% obstructions) {
+    direction <- turn_mat %*% direction
+  } else {
+    guard <- guard + direction
+  }
+  return(list(guard, direction))
+}
