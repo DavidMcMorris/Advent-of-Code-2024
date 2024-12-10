6da238959f545158a67ef1543a972f2d9d373ac5
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-10 14:37:49 -0500

    added merge sort functions

--- Day 5/Day5.R
+++ Day 5/Day5.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-input_filename <- "input.txt"
+input_filename <- "sample.txt"
 input <- readLines(input_filename)
 linebreak <- which(input == "")
 rules <- input[1:(linebreak - 1)] %>%
@@ -51,3 +51,44 @@ print(total)
 # Part 2
 
 out_of_order <- pages[-correct_order_inds]
+
+# Merge sort functions
+
+merge_lists <- function(left, right) {
+  result <- list()
+  while (length(left) * length(right) > 0) {
+    if (compare(left[[1]], right[[1]])) {
+      result <- c(result, left[1])
+      left <- left[!(left %in% result)]
+    } else {
+      result <- c(result, right[1])
+      right <- right[!(right %in% result)]
+    }
+  }
+  if (length(left) > 0) {
+    result <- c(result, left)
+  }
+  if (length(right) > 0) {
+    result <- c(result, right)
+  }
+  return(result)
+}
+
+merge_sort <- function(m) {
+  len <- length(m)
+  if (length(m) <= 1) {
+    return(m)
+  } else {
+    split_ind <- floor(len / 2)
+    left <- m[1:split_ind]
+    right <- m[(split_ind + 1) : len]
+
+    left <- merge_sort(left)
+    right <- merge_sort(right)
+
+    return(merge_lists(left, right))
+  }
+}
+
+
+out_of_order <- merge_sort(out_of_order)
