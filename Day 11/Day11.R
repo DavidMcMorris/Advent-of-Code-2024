863a798becd5e3cfb5068dd9aa08f9ac7561ce31
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-12 08:49:19 -0500

    Works for samples

---
+++ Day 11/Day11.R
@@ -0,0 +1,35 @@
+library(dplyr)
+
+input_filename <- "sample.txt"
+input <- scan(input_filename)
+
+blink <- function(stone) {
+  if (stone == 0) {
+    new_stones <- 1
+  } else if (nchar(stone) %% 2 == 0) {
+    mid <- (nchar(stone)) / 2
+    digits <- as.character(stone) %>%
+      strsplit(split = "") %>%
+      unlist()
+    new_stones <- digits[1:mid] %>%
+      paste0(collapse = "") %>%
+      as.numeric()
+    new_stones <- digits[-(1:mid)]  %>%
+      paste0(collapse = "") %>%
+      as.numeric() %>%
+      c(new_stones, .)
+  } else {
+    new_stones <- stone * 2024
+  }
+  return(new_stones)
+}
+
+for (i in 1:25) {
+  new_stones <- NULL
+  for (i in seq_along(input)) {
+    new_stones <- c(new_stones, blink(input[i]))
+  }
+  input <- new_stones
+}
+
+print(length(input))
