1a1027fe6cf9651c229276404fcc0f7c24a49c7d
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-13 09:31:49 -0500

    Works for sample but not for input

--- Day 13/Day13.R
+++ Day 13/Day13.R
@@ -2,3 +2,23 @@ library(dplyr)
 
 input_filename <- "input.txt"
 input <- readLines(input_filename)
+num_machines <- length(input)
+
+inds <- gregexpr("\\d+", input)
+nums <- regmatches(input, inds) %>%
+  unlist() %>%
+  as.numeric()
+
+tol <- 1e-10
+cost <- 0
+
+for (i in seq_len(num_machines)) {
+  a <- matrix(nums[i : (i + 3)], nrow = 2)
+  b <- matrix(nums[(i + 4) : (i + 5)])
+  x <- solve(a, b)
+  if (sum(unlist(lapply(x, function(y) min(abs(c(y %% 1, y %% 1 - 1))) < tol))) == 2) {
+    cost <- cost + 3 * x[1] + x[2]
+  }
+}
+
+print(cost)
