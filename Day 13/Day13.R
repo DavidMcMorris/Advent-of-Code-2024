0729928ca21be926c2ba7f586819a347dcf5c53d
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2024-12-13 13:27:20 -0500

    Adjusted the tolerance a few times until it gave the correct answer

--- Day 13/Day13.R
+++ Day 13/Day13.R
@@ -9,13 +9,13 @@ nums <- regmatches(input, inds) %>%
   unlist() %>%
   as.numeric()
 
-tol <- 1e-5
+tol <- 1e-3
 cost <- 0
 
 for (i in seq_len(num_machines)) {
   j <- 1 + 6 * (i - 1)
   a <- matrix(nums[j : (j + 3)], nrow = 2)
-  b <- matrix(nums[(j + 4) : (j + 5)])
+  b <- matrix(nums[(j + 4) : (j + 5)]) + 1e+13
   x <- solve(a, b)
   if (sum(unlist(lapply(x, function(y) min(abs(c(y %% 1, y %% 1 - 1))) < tol))) == 2) {
     cost <- cost + 3 * x[1] + x[2]
