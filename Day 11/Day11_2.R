6880244bf39eb707f2814d3145f947902e86b9a1
DavidMcMorris <david.mcmorris92@gmail.com>
2025-11-25 12:25:02 -0500

    Trying a mix of brute force and tracking 1's and 0's.

--- Day 11/Day11_2.R
+++ Day 11/Day11_2.R
@@ -2,4 +2,66 @@ library(dplyr)
 
 input_filename <- "input.txt"
 input <- scan(input_filename)
+one <- NULL
+zero <- NULL
 
+blink <- function(stone) {
+  if (stone == 0) {
+    new_stones <- 1
+  } else if (nchar(stone) %% 2 == 0) {
+    mid <- (nchar(stone)) / 2
+    digits <- as.character(stone) %>%
+      strsplit(split = "") %>%
+      unlist()
+    new_stones <- digits[1:mid] %>%
+      paste0(collapse = "") %>%
+      as.numeric()
+    new_stones <- digits[-(1:mid)]  %>%
+      paste0(collapse = "") %>%
+      as.numeric() %>%
+      c(new_stones, .)
+  } else {
+    new_stones <- stone * 2024
+  }
+  return(new_stones)
+}
+
+input1 <- 1
+for(i in 1:25) {
+#   print(i)
+  new_stones <- NULL
+  for (j in seq_along(input1)) {
+    new_stones <- c(new_stones, blink(input1[j]))
+    one[i] <- length(new_stones)
+  }
+  input1 <- new_stones
+}
+
+input0 <- 0
+for(i in 1:25) {
+#   print(i)
+  new_stones <- NULL
+  for (j in seq_along(input0)) {
+    new_stones <- c(new_stones, blink(input0[j]))
+    zero[i] <- length(new_stones)
+  }
+  input0 <- new_stones
+}
+
+onezero <- 0
+for(i in 1:75){
+  print(i)
+  new_stones <- NULL
+  for (j in seq_along(input)) {
+    if(input[j] == 1 || input[j] == 1){
+        if(input[j] == 1){
+            onezero <- onezero + one[26-i]
+        } else {
+            onezero <- onezero + one[26-i]
+        }
+    } else {
+    new_stones <- c(new_stones, blink(input[j]))
+    }
+  }
+  input <- new_stones
+}
